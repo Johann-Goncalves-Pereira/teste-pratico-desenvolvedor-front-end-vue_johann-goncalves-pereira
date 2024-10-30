0e2dba56bdae77ae6acd4f177ed49dab5f6fff24
Johann Goncalves Pereira <johann.gon.pereira@gmail.com>
2024-10-30 16:33:06 -0300

    feat: separantion of form from signin

---
+++ src/components/FormAddresses/FormAddresses.vue
@@ -0,0 +1,228 @@
+<script setup lang="ts">
+import { useAddressesStore } from '@/stores/address'
+import { useStateSuggestions } from '@/stores/suggestions'
+import { defineProps } from 'vue'
+import FormField from '../SignIn/FormField/FormField.vue'
+import InputField from '../SignIn/FormField/InputField.vue'
+
+const props = defineProps({
+	index: {
+		type: Number,
+		required: true,
+	},
+	push: Boolean,
+})
+
+const suggestions = useStateSuggestions()
+const addresses = useAddressesStore()
+const address = addresses.$state[props.index] || []
+</script>
+
+<template>
+	<form
+		class="form"
+		@submit.prevent="
+			() => {
+				addresses.pushAddress(addresses.$state, props.push, props.index)
+			}
+		"
+	>
+		<fieldset class="form__wrapper">
+			<legend class="form__title">
+				{{ $t('sing_in.title') }}
+				<p>
+					{{ address.logradouro.value }}
+					- {{ address.numero.value }}, {{ address.complemento }}
+					{{ address.bairro }}, {{ address.localidade.value }} -
+					{{ address.uf.value }} / {{ address.estado }}
+				</p>
+			</legend>
+
+			<FormField
+				required
+				:id="$t('sing_in.form.cep.id')"
+				:label="$t('sing_in.form.cep.label')"
+				:error="address.cep.valid"
+			>
+				<InputField
+					required
+					:id="$t('sing_in.form.cep.id')"
+					:placeholder="$t('sing_in.form.cep.placeholder')"
+					v-bind:value="address.cep.formatted"
+					v-on:input="addresses.handleCEP($event.target?.value, props.index)"
+					v-on:blur="
+						() => {
+							addresses.fetchAddressByCep(props.index)
+
+							if (address.cep.value.length === 0) {
+								address.cep.valid = 'Erro: CEP não pode ser um campo vazio'
+							}
+							if (address.cep.value.length !== 9) {
+								address.cep.valid = 'Erro: CEP deve ter 8 digitos'
+							}
+							if (address.cep.value.length === 8) {
+								address.cep.valid = ''
+							}
+						}
+					"
+				/>
+			</FormField>
+
+			<FormField
+				required
+				:id="$t('sing_in.form.uf.id')"
+				:label="$t('sing_in.form.uf.label')"
+				:error="address.uf.valid"
+			>
+				<InputField
+					required
+					list="state-uf-list"
+					:id="$t('sing_in.form.uf.id')"
+					:placeholder="$t('sing_in.form.uf.placeholder')"
+					v-bind:value="address.uf.value"
+					v-on:blur="
+						$event => {
+							const state = suggestions.states.find(
+								state => state.sigla === $event.target?.value,
+							)
+
+							if (state) {
+								address.uf.value = state?.sigla || ''
+								address.uf.valid = ''
+								address.estado = state?.nome || ''
+							} else {
+								address.uf.valid = 'Erro: Campo UF não existe'
+							}
+
+							if ($event.target?.value.length === 0) {
+								address.uf.valid = 'Erro: Campo UF está vazio'
+							}
+						}
+					"
+					v-on:mouseover="suggestions.fetchStateSuggestions()"
+				/>
+				<datalist id="state-uf-list">
+					<option
+						v-for="state in suggestions.states"
+						:key="state.sigla"
+						:value="`${state.sigla}`"
+					>
+						{{ state.nome }}
+					</option>
+				</datalist>
+			</FormField>
+
+			<FormField
+				required
+				:id="$t('sing_in.form.localidade.id')"
+				:label="$t('sing_in.form.localidade.label')"
+				:error="address.localidade.valid"
+			>
+				<InputField
+					required
+					list="city-list"
+					:id="$t('sing_in.form.localidade.id')"
+					:placeholder="$t('sing_in.form.localidade.placeholder')"
+					v-bind:value="address.localidade.value"
+					v-on:input="address.localidade.valid = $event.target?.value"
+					v-on:blur="
+						() => {
+							if (address.localidade.valid.length === 0) {
+								address.localidade.valid =
+									'Erro: Cidade não pode ser um campo vazio'
+							} else {
+								address.localidade.valid = ''
+							}
+						}
+					"
+					v-on:mouseover="suggestions.fetchCitySuggestions(address.uf.value)"
+				/>
+				<datalist id="city-list">
+					<option
+						v-for="city in suggestions.cities"
+						:key="city.id"
+						:value="city.nome"
+					>
+						{{ city.nome }}
+					</option>
+				</datalist>
+			</FormField>
+
+			<FormField
+				required
+				:id="$t('sing_in.form.logradouro.id')"
+				:label="$t('sing_in.form.logradouro.label')"
+				:error="address.logradouro.valid"
+			>
+				<InputField
+					required
+					:id="$t('sing_in.form.logradouro.id')"
+					:placeholder="$t('sing_in.form.logradouro.placeholder')"
+					v-bind:value="address.logradouro.value"
+					v-on:input="
+						$event => {
+							address.logradouro = $event.target?.value
+						}
+					"
+					v-on:blur="
+						() => {
+							if (address.logradouro.value.length === 0) {
+								address.logradouro.valid =
+									'Erro: Logradouro não pode ser um campo vazio'
+							} else {
+								address.logradouro.valid = ''
+							}
+						}
+					"
+				/>
+			</FormField>
+
+			<FormField
+				required
+				:id="$t('sing_in.form.numero.id')"
+				:label="$t('sing_in.form.numero.label')"
+				:error="address.numero.valid"
+			>
+				<InputField
+					type="number"
+					maxlength="6"
+					required
+					:id="$t('sing_in.form.numero.id')"
+					:placeholder="$t('sing_in.form.numero.placeholder')"
+					v-bind:value="address.numero"
+					v-on:input="
+						$event => {
+							address.numero.value = $event.target?.value
+								.match(/^\d+$/, '')
+								?.join('')
+						}
+					"
+				/>
+			</FormField>
+
+			<FormField
+				:id="$t('sing_in.form.complemento.id')"
+				:label="$t('sing_in.form.complemento.label')"
+			>
+				<InputField
+					:id="$t('sing_in.form.complemento.id')"
+					:placeholder="$t('sing_in.form.complemento.placeholder')"
+					v-bind:value="address.complemento"
+					v-on:input="address.complemento = $event.target?.value"
+				/>
+			</FormField>
+
+			<label class="sr-only" :for="$t('sing_in.form.submit.id')">Avançar</label>
+			<input
+				type="submit"
+				value="Avancar"
+				:id="$t('sing_in.form.submit.id')"
+				:name="$t('sing_in.form.submit.id')"
+			/>
+		</fieldset>
+	</form>
+</template>
+
+<style lang="scss" scoped>
+@use './FormAddresses.scss';
+</style>
--- src/stores/address.ts
+++ src/stores/address.ts
@@ -28,78 +28,6 @@ export type AddressFieldsProps = {
 	}
 }
 
-export const useAddressStore = defineStore('address', {
-	state: (): AddressFieldsProps => {
-		return {
-			cep: {
-				value: '',
-				formatted: '',
-				valid: '',
-			},
-			logradouro: { value: '', valid: '' },
-			complemento: '',
-			bairro: '',
-			localidade: {
-				value: '',
-				valid: '',
-			},
-			uf: {
-				value: '',
-				valid: '',
-			},
-			estado: '',
-			numero: {
-				value: '',
-				valid: '',
-			},
-		}
-	},
-	actions: {
-		handleCEP(formatted: string) {
-			this.cep.formatted = formatted
-				.replace(/[^0-9]/g, '')
-				.slice(0, 8)
-				.replace(/(\d{5})(\d{3})/, '$1-$2')
-
-			this.cep.value = this.cep.formatted.replace(/[^0-9]/g, '')
-		},
-		async fetchAddressByCep() {
-			if (this.cep.value.length !== 8) return
-
-			try {
-				const { data } = await axios.get(
-					`https://viacep.com.br/ws/${this.cep.value}/json/`,
-				)
-				console.log(data)
-
-				if (!data) {
-					this.cep.valid = 'Erro na busca do banco de dados'
-					throw new Error('Erro na busca do banco de dados')
-				}
-
-				const { logradouro, bairro, localidade, uf, complemento, estado } = data
-
-				if (!logradouro || !localidade || !uf) {
-					this.cep.valid = 'CEP só deu um endereço vazio'
-					throw new Error('CEP só deu um endereço vazio')
-				}
-
-				this.logradouro.value = logradouro || ''
-				this.complemento = complemento || ''
-				this.bairro = bairro || ''
-				this.localidade.value = localidade || ''
-				this.uf.value = uf || ''
-				this.estado = estado || ''
-			} catch (err) {
-				if (err instanceof Error) {
-					this.cep.valid = err.message
-				}
-				throw err
-			}
-		},
-	},
-})
-
 export const useAddressesStore = defineStore('addresses', {
 	state: (): AddressFieldsProps[] => {
 		return [
@@ -129,15 +57,66 @@ export const useAddressesStore = defineStore('addresses', {
 		]
 	},
 	actions: {
-		pushAddress(address: AddressFieldsProps | null) {
-			if (!address) return
-			if (address.cep?.value?.length !== 8) throw new Error('CEP inválido')
-			if (address.logradouro.valid?.length !== 0)
-				throw new Error('Logradouro inválido')
-			if (address.uf.valid?.length !== 0) throw new Error('UF inválido')
-			if (address.numero?.valid.length !== 0) throw new Error('Numero inválido')
+		handleCEP(formatted: string, index: number) {
+			this[index].cep.formatted = formatted
+				.replace(/[^0-9]/g, '')
+				.slice(0, 8)
+				.replace(/(\d{5})(\d{3})/, '$1-$2')
 
-			this.$state.push(address)
+			this[index].cep.value = this[index].cep.formatted.replace(/[^0-9]/g, '')
+		},
+		async fetchAddressByCep(index: number) {
+			if (this[index].cep.value.length !== 8) return
+
+			try {
+				const { data } = await axios.get(
+					`https://viacep.com.br/ws/${this[index].cep.value}/json/`,
+				)
+				console.log(data)
+
+				if (!data) {
+					this[index].cep.valid = 'Erro na busca do banco de dados'
+					throw new Error('Erro na busca do banco de dados')
+				}
+
+				const { logradouro, bairro, localidade, uf, complemento, estado } = data
+
+				if (!logradouro || !localidade || !uf) {
+					this[index].cep.valid = 'CEP só deu um endereço vazio'
+					throw new Error('CEP só deu um endereço vazio')
+				}
+
+				this[index].logradouro.value = logradouro || ''
+				this[index].complemento = complemento || ''
+				this[index].bairro = bairro || ''
+				this[index].localidade.value = localidade || ''
+				this[index].uf.value = uf || ''
+				this[index].estado = estado || ''
+			} catch (err) {
+				if (err instanceof Error) {
+					this[index].cep.valid = err.message
+				}
+				throw err
+			}
+		},
+		pushAddress(
+			address: AddressFieldsProps[],
+			pushOrEdit: boolean,
+			index: number,
+		) {
+			if (address[index].cep?.value?.length !== 8)
+				throw new Error('CEP inválido')
+			if (address[index].logradouro.valid?.length !== 0)
+				throw new Error('Logradouro inválido')
+			if (address[index].uf.valid?.length !== 0) throw new Error('UF inválido')
+			if (address[index].numero?.valid.length !== 0)
+				throw new Error('Numero inválido')
+
+			if (pushOrEdit) {
+				address.push(address[index])
+			} else {
+				address[index] = address[index]
+			}
 		},
 	},
 })
